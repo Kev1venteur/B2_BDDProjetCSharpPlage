bf349cd00bb3e2724ea618c904f316388aef483f
Kev1venteur <kvin.gillet@gmail.com>
2020-04-20 14:40:49 +0200

    Correction du trigger pour gérer les insertions multiples, avec un curseur.

--- Patchs/20200420 - #20 Table de laison ZP - Especes a un probleme avec le trigger/Correction du trigger - ajout d'une variable (ajout de la nouvelle colonne).sql
+++ Patchs/20200420 - #20 Table de laison ZP - Especes a un probleme avec le trigger/Correction du trigger - ajout d'une variable (ajout de la nouvelle colonne).sql
@@ -1,16 +1,23 @@
 USE [Betterave]
 GO
-/****** Object:  Trigger [dbo].[PasDuplicationComptage]    Script Date: 20/04/2020 14:15:49 ******/
+
+/****** Object:  Trigger [PasDuplicationComptage]    Script Date: 20/04/2020 14:39:32 ******/
+DROP TRIGGER [dbo].[PasDuplicationComptage]
+GO
+
+/****** Object:  Trigger [dbo].[PasDuplicationComptage]    Script Date: 20/04/2020 14:39:32 ******/
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
+
 -- =============================================
 -- Author:		<Author,,Name>
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
-ALTER TRIGGER [dbo].[PasDuplicationComptage]
+CREATE TRIGGER [dbo].[PasDuplicationComptage]
    ON  [dbo].[Especes_Has_ZonePrelevements]
    INSTEAD OF INSERT,UPDATE
 AS 
@@ -35,18 +42,35 @@ BEGIN
 					join inserted i
 					on i.idEspece = ezp.idEspece and i.idZone=ezp.idZone
 					where i.idEspece = ezp.idEspece and i.idZone = ezp.idZone)
+
 			begin
 				declare @idEspece int, @idZone int, @date date, @nbIndividus int
 
-				SET @idEspece = (select idEspece from inserted)
-				SET @idZone = (select idZone from inserted)
-				SET @date = (select dateRencontre from inserted)
-				SET @nbIndividus = (select nbIndividus from inserted)
+				declare cur cursor static local for
+					select idEspece, idZone, dateRencontre, nbIndividus from inserted
 
-				insert into Especes_Has_ZonePrelevements 
-				(idEspece, idZone, dateRencontre, nbIndividus)
-				VALUES (@idEspece,@idZone,@date,@nbIndividus);
+				open cur
 
+				fetch next from cur into @idEspece, @idZone, @date, @nbIndividus
+
+				while @@FETCH_STATUS=0
+
+				begin
+					insert into Especes_Has_ZonePrelevements 
+					(idEspece, idZone, dateRencontre, nbIndividus)
+					VALUES (@idEspece,@idZone,@date,@nbIndividus)
+
+					fetch next from cur into @idEspece, @idZone, @date, @nbIndividus
+				end
+
+				close cur
+				deallocate cur
 			end
 		end
 END
+GO
+
+ALTER TABLE [dbo].[Especes_Has_ZonePrelevements] ENABLE TRIGGER [PasDuplicationComptage]
+GO
+
+
